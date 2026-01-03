cb869a894ecff4a0fcf43c18f587bd89837d6ed2
PrayagSrinivas <sprayagsahu@gmail.com>
2026-01-03 14:35:42 +0530

    Switch to binary distribution 1.0.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,38 @@
 // swift-tools-version: 5.9
+//import PackageDescription
+//
+//let package = Package(
+//    name: "NetworkSnifffer",
+//    // 1. IMPORTANT: Define the minimum platform version.
+//    // iOS 14 is safe for modern SwiftUI. Use .v15 if you need newer features.
+//    platforms: [
+//        .iOS(.v15)
+//    ],
+//    products: [
+//        .library(
+//            name: "NetworkSnifffer",
+//            type: .dynamic,
+//            targets: ["NetworkSnifffer"]
+//        ),
+//    ],
+//    targets: [
+//        // This target looks for code in 'Sources/NetworkSnifffer'
+//        .target(
+//            name: "NetworkSnifffer",
+//            dependencies: []
+//        ),
+//        .testTarget(
+//            name: "NetworkSniffferTests",
+//            dependencies: ["NetworkSnifffer"]
+//        ),
+//    ]
+//)
+
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "NetworkSnifffer",
-    // 1. IMPORTANT: Define the minimum platform version.
-    // iOS 14 is safe for modern SwiftUI. Use .v15 if you need newer features.
     platforms: [
         .iOS(.v15)
     ],
@@ -15,14 +43,12 @@ let package = Package(
         ),
     ],
     targets: [
-        // This target looks for code in 'Sources/NetworkSnifffer'
-        .target(
+        .binaryTarget(
             name: "NetworkSnifffer",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "NetworkSniffferTests",
-            dependencies: ["NetworkSnifffer"]
-        ),
+            url: "https://github.com/PrayagSrinivas/NetworkSnifffer/releases/download/1.0.0/NetworkSnifffer.xcframework.zip",
+            
+            // 2. PASTE YOUR CHECKSUM HERE 👇
+            checksum: "b53c161602c1d98a53772b41b9659332505daa619f555fc974fe5836ff0d6214"
+        )
     ]
 )
